b5d0aad7c04174d5ad60bc80ecb8d713543bdd0b
miguelmoura247-oss <miguelmoura247@gmail.com>
2026-08-24 16:35:50 -0300

    atividade

---
+++ projeto/app.js
@@ -0,0 +1,13 @@
+import {
+    celsiusParaFahrenheit,
+    horasParaMinutos,
+    idadeEmDias,
+    kmParaMetros,
+    consumoMedia
+} from "./conversores.js";
+
+console.log(celsiusParaFahrenheit(30));
+console.log(horasParaMinutos(2));
+console.log(idadeEmDias(20));
+console.log(kmParaMetros(3));
+console.log(consumoMedio(500, 40));
---
+++ projeto/conversores.js
@@ -0,0 +1,36 @@
+
+    function celsiusParaFahrenheit(celsius){
+    let F = (9 / 5) * celsius + 32;
+    return F;
+ }
+
+
+ function horasparaminutos(horas){
+    let minutos = horas * 60;
+    return minutos;
+ }
+
+
+ function idadeEmDias(idade){
+    let dias = idade *365;
+    return idade;
+ }
+
+
+ function kmParaMetros(km){
+    let metros = km * 1000;
+    return metros;
+ }
+
+ function consumoMedia(distancia, Litros){
+    let consumo = distancia / Litros;
+    return consumoMedia;
+ }
+ export{
+    celsiusParaFahrenheit,
+    horasparaminutos,
+    idadeEmDias,
+    kmParaMetros,
+    consumoMedia
+ };
+
